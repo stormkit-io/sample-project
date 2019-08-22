2f38e9f1bcc2e36cae3efb10096141579bb4ee50
Savas Vedova <savas@vsk.me>
2019-08-22 17:45:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is your first app on Stormkit.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.stormkit.io/docs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read the docs
         </a>
       </header>
     </div>
