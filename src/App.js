3c55c50b799df50541a96b94a34ccbc644bec5db
Savas Vedova <savas@vsk.me>
2019-08-24 12:07:40 +0200

    Add social links

--- src/App.js
+++ src/App.js
@@ -6,26 +6,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is your first app on Stormkit.</p>
-        <div>
+        <div className="App-content">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>This is your first app on Stormkit.</p>
+          <div className="App-links">
+            <a
+              className="App-link"
+              href="https://www.stormkit.io/docs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Read the docs
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://github.com/stormkit-dev/sample-project"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="App-link"
+            >
+              Fork the repo
+            </a>{" "}
+          </div>
+        </div>
+        <div className="App-social">
           <a
-            className="App-link"
-            href="https://www.stormkit.io/docs"
+            href="https://twitter.com/stormkitio"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer noopener"
           >
-            Read the docs
-          </a>{" "}
-          |{" "}
+            <i className="fab fa-twitter" />
+          </a>
           <a
-            href="https://github.com/stormkit-dev/sample-project"
+            href="https://www.youtube.com/channel/UC6C_-UuAiIWlGOIokT03lRQ"
             target="_blank"
-            rel="noopener noreferrer"
-            className="App-link"
+            rel="noreferrer noopener"
           >
-            Fork the repo
-          </a>{" "}
+            <i className="fab fa-youtube" />
+          </a>
         </div>
       </header>
     </div>
