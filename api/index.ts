933565efaa519b7c2a1b1efdbd01abfc9502e7c8
Savas Vedova <savas@stormkit.io>
2023-10-03 20:23:26 +0300

    chore: minor improvements

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import * as http from "http";
+import * as http from "node:http";
 import fetch from "node-fetch";
 
 export default (req: http.IncomingMessage, res: http.ServerResponse) => {
@@ -8,16 +8,30 @@ export default (req: http.IncomingMessage, res: http.ServerResponse) => {
     referrerPolicy: "strict-origin-when-cross-origin",
     body: null,
     method: "GET",
-  }).then(async (data) => {
-    const text = await data.text();
+  })
+    .then(async (data) => {
+      const text = await data.text();
 
-    res.setHeader("Content-Type", "application/json");
-    res.write(
-      JSON.stringify({
-        apiVersion: text.split("Version: ")[1].split(/\s/)[0],
-      })
-    );
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({
+          apiVersion: text.split("Version: ")[1].split(/\s/)[0],
+        })
+      );
 
-    res.end();
-  });
+      res.end();
+    })
+    .catch((e) => {
+      console.log(e);
+
+      res.setHeader("Content-Type", "application/json");
+      res.write(
+        JSON.stringify({
+          error:
+            "Something went wrong while fetching api version. Check the runtime logs for more information.",
+        })
+      );
+
+      res.end();
+    });
 };
