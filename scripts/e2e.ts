e8463f6121ff768e8831726ee21c3e236bd71606
Savas Vedova <savas@stormkit.io>
2024-09-09 20:09:49 +0300

    feat: introduce status checks

---
+++ scripts/e2e.ts
@@ -0,0 +1,22 @@
+const endpoint = process.env.SK_DEPLOYMENT_URL;
+
+if (!endpoint) {
+  throw new Error("SK_DEPLOYMENT_URL environment variable is missing.");
+}
+
+fetch(endpoint)
+  .then(async (data) => {
+    const content = await data.text();
+
+    if (content.includes("<title>Stormkit | Sample Project</title>")) {
+      console.log("Success!");
+      process.exit(0);
+    } else {
+      console.log("Title is missing");
+      process.exit(1);
+    }
+  })
+  .catch(() => {
+    console.log("Error while making request");
+    process.exit(1);
+  });
