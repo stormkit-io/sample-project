439aa4889367de1d8bf327b3d140c5ca96c86e21
Savas Vedova <savas@stormkit.io>
2025-12-25 08:48:10 +0300

    chore: seed cities using sql

--- migrations/04.add_cities_table.up.sql
+++ migrations/04.add_cities_table.up.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS cities (
     established DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO cities (name, country, population, established)
+VALUES 
+    ('New York', 'United States', 8336817, '1624-07-01'),
+    ('Tokyo', 'Japan', 13960000, '1603-01-01'),
+    ('London', 'United Kingdom', 8982000, '0043-01-01'),
+    ('Paris', 'France', 2161000, '0250-01-01'),
+    ('Sydney', 'Australia', 5312000, '1788-01-26')
+ON CONFLICT DO NOTHING;
